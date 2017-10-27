9c3496608c759f896c52a865dd24b722eb168f75
Gwyn Jones <m@gwnjns.com>
2017-10-27 17:20:27 +0100

    Introduce script tags for dependencies
    This feels like a strange thing to do but, on balance, it seems the
    most sensible approach given the nature of this app and where it's
    to be used.

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Browser capability detector</title>
+    <script src="scripts/modernizr.js"></script>
 </head>
 <body>
-
+<script src="https://code.jquery.com/jquery-1.12.4.js" integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU=" crossorigin="anonymous"></script>
+<script src="scripts/all_features_detected_by_modernizr.js"></script>
+<script src="scripts/app.js"></script>
 </body>
 </html>
