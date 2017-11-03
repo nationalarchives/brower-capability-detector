a3dc97c9d79ac9a608aec18aeb71304145ece51d
Gwyn Jones <m@gwnjns.com>
2017-11-03 17:45:13 +0000

    Remove minification since this is breaking IE8
    Related GruntJS task is removed and instead the concatenation task
    simply results in a file with the .min.js extension (since that is
    what is consumed by the page).

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,15 +9,6 @@ module.exports = function (grunt) {
             },
             dist: {
                 src: ['scripts/modernizr.js', 'scripts/all_features_detected_by_modernizr.js', 'scripts/browser_capabilities.js'],
-                dest: 'scripts/app.js'
-            }
-        },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'scripts/app.js',
                 dest: 'scripts/app.min.js'
             }
         },
@@ -32,11 +23,10 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['concat']);
 
 };
